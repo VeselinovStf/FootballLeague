57b344ce2b2ff67286f6006cb738d9d0d23bc8d0
veselinovStf <veselinovStf@gmail.com>
2022-10-10 09:03:56 +0300

    Adding Red-Test for GetAllTeamsRankingAsync TeamService

---
+++ tests/Business/FootballLeague.CoreTests/Stubs/TeamStub.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballLeague.CoreTests.Stubs
+{
+    public static class TeamStub
+    {
+        public static List<Team> GetTeamsWithStatisticsStub()
+        {
+            return new List<Team>()
+            {
+                new Team()
+                {
+                    Id = 1,
+                    Name = "Team 1",
+                    Statistics = new Statistic()
+                    {
+                        Id = 1,
+                        TotalScore = 5
+                    }
+                },
+                new Team()
+                {
+                    Id = 2,
+                    Name = "Team 2",
+                    Statistics = new Statistic()
+                    {
+                        Id = 2,
+                        TotalScore = 78
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ tests/Business/FootballLeague.CoreTests/TeamService_Should.cs
@@ -0,0 +1,27 @@
+﻿using FootballLeague.CoreTests.Stubs;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FootballLeague.CoreTests
+{
+    public class TeamService_Should
+    {     
+        [Test]
+        public async Task GetAllTeamsRanking()
+        {
+            var expectedTeamsWithRanking = TeamStub.GetTeamsWithStatisticsStub();
+
+            var teamServiceAsyncRepositoryMock = new Mock<IAsyncRepository<Team>>();
+            teamServiceAsyncRepositoryMock.Setup(m => m.GetAllBySpec()).Returns(expectedTeamsWithRanking);
+
+            var teamService = new TeamService(teamServiceAsyncRepositoryMock.Object);
+
+            var actualTeamsWithRanking = await teamService.GetAllTeamsRankingAsync();
+
+            Assert.NotNull(actualTeamsWithRanking);
+            CollectionAssert.AreEqual(expectedTeamsWithRanking, actualTeamsWithRanking);
+        }
+    }
+}
