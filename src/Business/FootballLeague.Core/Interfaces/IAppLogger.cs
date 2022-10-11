4cf467c03aca50fc2a5c369ccdd978789dcc5ab5
veselinovStf <veselinovStf@gmail.com>
2022-10-11 08:52:49 +0300

    Adding Application Logger

---
+++ src/Business/FootballLeague.Core/Interfaces/IAppLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace FootballLeague.Core.Interfaces
+{
+    public interface IAppLogger<T>
+    {
+        void LogInfo(string message, params object[] args);
+        void LogWarn(string message, params object[] args);
+        void LogError(string message, params object[] args);
+    }
+}
---
+++ src/Infrastructure/FootballLeague.Infrastructure/Logging/AppLoggerAdapter.cs
@@ -0,0 +1,29 @@
+﻿using FootballLeague.Core.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace FootballLeague.Infrastructure.Logging
+{
+    public class AppLoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+        public AppLoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogInfo(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarn(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+        }
+    }
+}
