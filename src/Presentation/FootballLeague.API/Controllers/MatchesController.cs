fa821c9880e2dde73a77b5ce8b1ebd7c9e38a7ca
veselinovStf <veselinovStf@gmail.com>
2022-10-11 10:22:08 +0300

    Adding Matches Controller

---
+++ src/Presentation/FootballLeague.API/Controllers/MatchesController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace FootballLeague.API.Controllers
+{
+    public class MatchesController : BaseApiController
+    {
+        private readonly IMediator _mediator;
+
+        public MatchesController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+    }
+}
