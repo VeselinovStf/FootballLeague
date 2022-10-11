1732b5902ab6142284151d4e8228cd40bfcbec7a
veselinovStf <veselinovStf@gmail.com>
2022-10-11 09:17:59 +0300

    Adding NLOG

--- src/Presentation/FootballLeague.API/Program.cs
+++ src/Presentation/FootballLeague.API/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System;
+using NLog.Web;
+using NLog;
 
 namespace FootballLeague.API
 {
@@ -7,14 +12,41 @@ namespace FootballLeague.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logsPath = Path.Combine(Environment.CurrentDirectory, "Logs");
+
+            NLog.GlobalDiagnosticsContext.Set("AppDirectory", logsPath);
+            NLog.Common.InternalLogger.LogFile = Path.Combine(logsPath, "nlog-internal.log");
+
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                    logging.ClearProviders();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).UseNLog();
     }
 }
