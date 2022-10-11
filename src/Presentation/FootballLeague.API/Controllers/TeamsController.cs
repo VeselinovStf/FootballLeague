778508da014985d8ba48669d640f45bb48aeec3e
veselinovStf <veselinovStf@gmail.com>
2022-10-11 09:27:01 +0300

    Adding Team Controller

---
+++ src/Presentation/FootballLeague.API/Controllers/TeamsController.cs
@@ -0,0 +1,26 @@
+﻿using FootballLeague.API.Features.Queries.Team;
+using FootballLeague.API.Features.Team.Queries.ResponseModels;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FootballLeague.API.Controllers
+{
+    public class TeamsController : BaseApiController
+    {
+        private readonly IMediator _mediator;
+
+        public TeamsController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpGet("rankings")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetTeamsRankingQueryResponseModel))]
+        public async Task<IActionResult> GetTeamsRankings()
+        {
+            return Ok(await this._mediator.Send(new GetTeamsRankingQuery()));
+        }
+    }
+}
