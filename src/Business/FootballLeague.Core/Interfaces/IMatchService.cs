24ae6aef437083f1c6b27cb866bd420e45371ba0
veselinovStf <veselinovStf@gmail.com>
2022-10-11 16:19:31 +0300

    Adding MatchService methods

--- src/Business/FootballLeague.Core/Interfaces/IMatchService.cs
+++ src/Business/FootballLeague.Core/Interfaces/IMatchService.cs
@@ -1,5 +1,6 @@
 ﻿using FootballLeague.Core.Entities;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FootballLeague.Core.Interfaces
@@ -12,5 +13,15 @@ namespace FootballLeague.Core.Interfaces
              DateTime matchDate,
              int homeTeamScore,
              int awayTeamScore);
+
+        Task<Match> GetMatchByIdAsync(int expectedId);
+        Task<IEnumerable<Match>> GetAllMatchesAsync();
+
+        Task UpdateMatchAsync(int id,
+            DateTime matchDate,
+            int homeTeamResult,
+            int awayTeamResult);
+
+        Task DeleteMatchAsync(int id);
     }
 }
--- src/Business/FootballLeague.Core/Services/MatchService.cs
+++ src/Business/FootballLeague.Core/Services/MatchService.cs
@@ -2,6 +2,7 @@
 using FootballLeague.Core.Interfaces;
 using FootballLeague.Core.Validations;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FootballLeague.Core.Services
@@ -45,6 +46,49 @@ namespace FootballLeague.Core.Services
             );
 
             return await this._matchRepository.AddAsync(newMatch);
+        }
+
+        public async Task DeleteMatchAsync(int id)
+        {
+            Guard.ValueLessThenEqual(0, id);
+
+            var currentMatch = await this._matchRepository.GetByIdAsync(id);
+
+            Guard.NotNull(currentMatch);
+
+            await this._matchRepository.DeleteAsync(currentMatch);
+        }
+
+        public async Task<IEnumerable<Match>> GetAllMatchesAsync()
+        {
+            return await this._matchRepository.ListAllAsync();
+        }
+
+        public async Task<Match> GetMatchByIdAsync(int expectedId)
+        {
+            Guard.ValueLessThenEqual(0, expectedId);
+
+            return await this._matchRepository.GetByIdAsync(expectedId);
+        }
+
+        public async Task UpdateMatchAsync(
+            int id, 
+            DateTime matchDate, 
+            int homeTeamResult, 
+            int awayTeamResult)
+        {
+            Guard.ValueLessThenEqual(0, id);
+            Guard.ValueLessThenEqual(-1, homeTeamResult);
+            Guard.ValueLessThenEqual(-1, awayTeamResult);
+
+            var currentMatch = await this._matchRepository.GetByIdAsync(id);
+
+            Guard.NotNull(currentMatch);
+
+            currentMatch.UpdateMatchResult(homeTeamResult, awayTeamResult);
+            currentMatch.UpdateMatchDate(matchDate);
+
+            await this._matchRepository.UpdateAsync(currentMatch);
         }
     }
 }
