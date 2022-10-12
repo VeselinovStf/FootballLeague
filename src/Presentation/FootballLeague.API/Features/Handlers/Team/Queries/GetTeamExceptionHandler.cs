b0024dc5eeafa57d71144130cdeb65492fec6f22
veselinovStf <veselinovStf@gmail.com>
2022-10-12 10:08:13 +0300

    Adding Exception Handler

---
+++ src/Presentation/FootballLeague.API/Features/Handlers/Team/Queries/GetTeamExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using FootballLeague.API.Features.Handlers.Team.Commands;
+using FootballLeague.API.Features.Queries.Team.ResponseModels;
+using FootballLeague.API.Features.Queries.Team;
+using FootballLeague.Core.Interfaces;
+using MediatR.Pipeline;
+using System;
+
+namespace FootballLeague.API.Features.Handlers.Team.Queries
+{
+    public class GetTeamExceptionHandler : RequestExceptionHandler<GetTeamQuery, GetTeamQueryResponseModel>
+    {
+        private readonly IAppLogger<DeleteTeamExceptionHandler> _appLogger;
+
+        public GetTeamExceptionHandler(IAppLogger<DeleteTeamExceptionHandler> appLogger)
+        {
+            this._appLogger = appLogger;
+        }
+        protected override void Handle(GetTeamQuery request, Exception exception, RequestExceptionHandlerState<GetTeamQueryResponseModel> state)
+        {
+            _appLogger.LogError(exception.Message, exception);
+
+            state.SetHandled(new GetTeamQueryResponseModel(false, "Team not found"));
+        }
+    }
+}
