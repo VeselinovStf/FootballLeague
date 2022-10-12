665841e48f5c7649b007ff3006fe1f5a01b42346
veselinovStf <veselinovStf@gmail.com>
2022-10-12 10:36:16 +0300

    Adding Create Exception Handlers

---
+++ src/Presentation/FootballLeague.API/Features/Handlers/Match/Commands/CreateMatchExceptionHandler.cs
@@ -0,0 +1,26 @@
+﻿using FootballLeague.API.Features.Commands.Match.ResponseModels;
+using FootballLeague.API.Features.Commands.Match;
+using MediatR;
+using MediatR.Pipeline;
+using System;
+using FootballLeague.API.Features.Handlers.Team.Commands;
+using FootballLeague.Core.Interfaces;
+
+namespace FootballLeague.API.Features.Handlers.Match.Commands
+{
+    public class CreateMatchExceptionHandler : RequestExceptionHandler<CreateMatchRequest, CreateMatchResponseModel>
+    {
+        private readonly IAppLogger<DeleteTeamExceptionHandler> _appLogger;
+
+        public CreateMatchExceptionHandler(IAppLogger<DeleteTeamExceptionHandler> appLogger)
+        {
+            this._appLogger = appLogger;
+        }
+        protected override void Handle(CreateMatchRequest request, Exception exception, RequestExceptionHandlerState<CreateMatchResponseModel> state)
+        {
+            _appLogger.LogError(exception.Message, exception);
+
+            state.SetHandled(new CreateMatchResponseModel(false, "Unable To Create Match"));
+        }
+    }
+}
---
+++ src/Presentation/FootballLeague.API/Features/Handlers/Team/Commands/CreateTeamExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using FootballLeague.API.Features.Commands.Team.ResponseModels;
+using FootballLeague.API.Features.Commands.Team;
+using MediatR.Pipeline;
+using System;
+using FootballLeague.Core.Interfaces;
+
+namespace FootballLeague.API.Features.Handlers.Team.Commands
+{
+    public class CreateTeamExceptionHandler : RequestExceptionHandler<CreateTeamRequest, CreateTeamResponseModel>
+    {
+        private readonly IAppLogger<DeleteTeamExceptionHandler> _appLogger;
+
+        public CreateTeamExceptionHandler(IAppLogger<DeleteTeamExceptionHandler> appLogger)
+        {
+            this._appLogger = appLogger;
+        }
+
+        protected override void Handle(CreateTeamRequest request, Exception exception, RequestExceptionHandlerState<CreateTeamResponseModel> state)
+        {
+            _appLogger.LogError(exception.Message, exception);
+
+            state.SetHandled(new CreateTeamResponseModel(false, "Unable to Create Team"));
+        }
+    }
+}
