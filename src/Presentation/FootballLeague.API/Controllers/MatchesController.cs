7a74780e2bd970ef50f6db5d94bb64cb968064aa
veselinovStf <veselinovStf@gmail.com>
2022-10-12 09:36:40 +0300

    Adding Authenticatication to controllers

--- src/Presentation/FootballLeague.API/Controllers/MatchesController.cs
+++ src/Presentation/FootballLeague.API/Controllers/MatchesController.cs
@@ -11,7 +11,6 @@ using System.Threading.Tasks;
 
 namespace FootballLeague.API.Controllers
 {
-    [Authorize(Roles = "ADMINISTRATOR")]
     public class MatchesController : BaseApiController
     {
         private readonly IMediator _mediator;
@@ -21,13 +20,16 @@ namespace FootballLeague.API.Controllers
             this._mediator = mediator;
         }
 
+        [Authorize(Roles = "ADMINISTRATOR")]
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CreateMatchResponseModel))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> CreateTeam(CreateMatchRequest request)
         {
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(request));
         }
 
+        [AllowAnonymous]
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetMatchesQueryResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(GetMatchesQueryResponseModel))]
@@ -36,6 +38,7 @@ namespace FootballLeague.API.Controllers
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(new GetMatchesQuery()));
         }
 
+        [AllowAnonymous]
         [HttpGet("id")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetMatchQueryResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(GetMatchQueryResponseModel))]
@@ -44,17 +47,21 @@ namespace FootballLeague.API.Controllers
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(new GetMatchQuery(id)));
         }
 
+        [Authorize(Roles = "ADMINISTRATOR")]
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UpdateMatchResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(UpdateMatchResponseModel))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> UpdateMatch(UpdateMatchRequest request)
         {
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(request));
         }
 
+        [Authorize(Roles = "ADMINISTRATOR")]
         [HttpDelete]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DeleteMatchResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(DeleteMatchResponseModel))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> DeleteMatch(DeleteMatchRequestModel request)
         {
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(request));
--- src/Presentation/FootballLeague.API/Controllers/TeamsController.cs
+++ src/Presentation/FootballLeague.API/Controllers/TeamsController.cs
@@ -5,12 +5,15 @@ using FootballLeague.API.Features.Queries.Team;
 using FootballLeague.API.Features.Queries.Team.ResponseModels;
 using FootballLeague.API.Features.Team.Queries.ResponseModels;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace FootballLeague.API.Controllers
 {
+   
     public class TeamsController : BaseApiController
     {
         private readonly IMediator _mediator;
@@ -20,6 +23,7 @@ namespace FootballLeague.API.Controllers
             this._mediator = mediator;
         }
 
+        [AllowAnonymous]
         [HttpGet("rankings")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetTeamsRankingQueryResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(GetTeamsRankingQueryResponseModel))]
@@ -29,6 +33,7 @@ namespace FootballLeague.API.Controllers
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(new GetTeamsRankingQuery()));
         }
 
+        [AllowAnonymous]
         [HttpGet("played")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetTeamsPlayedMatchesQueryResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(GetTeamsPlayedMatchesQueryResponseModel))]
@@ -38,6 +43,7 @@ namespace FootballLeague.API.Controllers
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(new GetTeamsPlayedMatchesQuery()));
         }
 
+        [AllowAnonymous]
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetTeamsQueryResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(GetTeamsQueryResponseModel))]
@@ -47,6 +53,7 @@ namespace FootballLeague.API.Controllers
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(new GetTeamsQuery()));
         }
 
+        [AllowAnonymous]
         [HttpGet("id")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetTeamQueryResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(GetTeamQueryResponseModel))]
@@ -56,25 +63,31 @@ namespace FootballLeague.API.Controllers
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(new GetTeamQuery(id)));
         }
 
+        [Authorize(Roles = "ADMINISTRATOR")]
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CreateTeamResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(CreateTeamResponseModel))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> CreateTeam(CreateTeamRequest request)
         {
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(request));
         }
 
+        [Authorize(Roles = "ADMINISTRATOR")]
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UpdateTeamResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(UpdateTeamResponseModel))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> UpdateTeam(UpdateTeamRequest request)
         {
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(request));
         }
 
+        [Authorize(Roles = "ADMINISTRATOR")]
         [HttpDelete]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DeleteTeamResponseModel))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(DeleteTeamResponseModel))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> DeleteTeam(DeleteTeamRequest request)
         {
             return ApiResponseExtensions.ValidateResponseModel(await this._mediator.Send(request));
